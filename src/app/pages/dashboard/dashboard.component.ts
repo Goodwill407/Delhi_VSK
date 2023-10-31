65544e794f2805464de934cccfac06a39d383fdb
unknown <devops@simplifiedschooling.in>
2023-10-31 18:28:36 +0530

    dashboard done with api

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { HttpServiceService } from 'src/app/services/http-service.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  allDistrictData: any =[];
 
+  totalCount:any = [];
+  constructor(private httpService: HttpServiceService) {}
 
+  ngOnInit(){
+    this.getSchoolByDistrict();
+    this.allTotalCount();
+  }
+
+  getSchoolByDistrict(){
+    this.httpService.get('graphs/school-student-count-by-district').subscribe((data: any) => {
+      if (data) {
+        this.allDistrictData =data;
+      }
+    })
+  }
+
+  allTotalCount(){
+    this.httpService.get('graphs').subscribe((data: any) => {
+      if (data) {
+        this.totalCount =data;
+      }
+    })
+  }
 }
 
