d9dda74430c019e249c0f6982807de85e6d53388
Goodwill407 <devops@simplifiedschooling.in>
2023-10-31 10:09:36 +0530

    code sharing of delhi

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import * as pbi from 'powerbi-client';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,74 +7,52 @@ import * as pbi from 'powerbi-client';
 })
 export class DashboardComponent {
 
-  @ViewChild('embeddedReport1') embeddedReport1!: ElementRef;
-  @ViewChild('embeddedReport2') embeddedReport2!: ElementRef;
-  @ViewChild('embeddedReport3') embeddedReport3!: ElementRef;
-  @ViewChild('embeddedReport4') embeddedReport4!: ElementRef;
-  @ViewChild('embeddedReport5') embeddedReport5!: ElementRef;
-  @ViewChild('embeddedReport6') embeddedReport6!: ElementRef;
-  @ViewChild('embeddedReport7') embeddedReport7!: ElementRef;
-  @ViewChild('embeddedReport8') embeddedReport8!: ElementRef;
-  @ViewChild('embeddedReport9') embeddedReport9!: ElementRef;
-  @ViewChild('embeddedReport10') embeddedReport10!: ElementRef;
-  @ViewChild('embeddedReport11') embeddedReport11!: ElementRef;
-  @ViewChild('embeddedReport12') embeddedReport12!: ElementRef;
-  @ViewChild('embeddedReport13') embeddedReport13!: ElementRef;
-  @ViewChild('embeddedReport14') embeddedReport14!: ElementRef;
+  commonBarGraph: any;
+  commonPieGraph: any;
 
-  reportConfig: any;
-
-  chartOptions: any;
-  chartOptions2: any;
+  teacherGenderRatio: any;
+  studentsGenderRatio: any;
+  typesOfSchools: any;
 
   constructor() {
-
-    this.chartOptions = {
-      series: [
-        {
-          name: "serie1",
-          data: [44, 55, 41, 64, 22, 43, 21]
-        },
-        {
-          name: "serie2",
-
-          data: [53, 32, 33, 52, 13, 44, 32]
-        }
-      ],
+    this.commonBarGraph = {
+      series: [],
       chart: {
         type: "bar",
-        height: 200
-      },
-      plotOptions: {
-        bar: {
-          vertical: true,
-          dataLabels: {
-            position: "top"
+        events: {
+          click: function (chart: any, w: any, e: any) {
+            // console.log(chart, w, e)
           }
         }
       },
-      dataLabels: {
-        enabled: true,
-        offsetX: -6,
-        style: {
-          fontSize: "12px",
-          colors: ["#fff"]
+      plotOptions: {
+        bar: {
+          columnWidth: "45%",
+          distributed: true
         }
       },
-      stroke: {
-        show: true,
-        width: 1,
-        colors: ["#fff"]
+      dataLabels: {
+        enabled: false
+      },
+      legend: {
+        show: false
+      },
+      grid: {
+        show: false
       },
       xaxis: {
-        categories: [2001, 2002, 2003, 2004, 2005, 2006, 2007]
+        categories: [],
+        labels: {
+          style: {
+            fontSize: "12px"
+          }
+        }
       }
     };
 
-    this.chartOptions2 = {
-      series: [44, 55, 41, 17, 15],
+    this.commonPieGraph = {
+      series: [],
       chart: {
-        height: 300,
         type: "donut"
       },
       dataLabels: {
@@ -83,6 +60,19 @@ export class DashboardComponent {
       },
       fill: {
         type: "gradient"
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            labels: {
+              show: true,
+              showAlways: false,
+              formatter: (w: any) => {
+                return ' tCO2e'
+              }
+            }
+          }
+        }
       },
       legend: {
         formatter: function (val: any, opts: any) {
@@ -102,34 +92,46 @@ export class DashboardComponent {
           }
         }
       ]
-    };
+    }
   }
 
-  async ngOnInit() {
-    this.reportConfig = await fetch('https://playgroundbe-bck-1.azurewebsites.net/Reports/SampleReport');
-    this.reportConfig = await this.reportConfig.json();
-    this.showReport();
+  ngOnInit() {
+    this.getStudentsGenderRatio();
+    this.getTeachersGenderRatio();
+    this.getTypesOfSchools();
   }
 
-  showReport() {
-
-    let config = {
-      type: 'report',
-      tokenType: pbi.models.TokenType.Embed,
-      accessToken: this.reportConfig.EmbedToken.Token,
-      embedUrl: this.reportConfig.EmbedUrl,
-      filters: [],
-      settings: {
-        filterPaneEnabled: true,
-        navContentPaneEnabled: true
-      }
-    };
-
-    let powerbi = new pbi.service.Service(
-      pbi.factories.hpmFactory,
-      pbi.factories.wpmpFactory,
-      pbi.factories.routerFactory
-    );
+  getStudentsGenderRatio() {
+    const series = [{
+      name: "Graphical",
+      data: [21, 22, 10, 28, 16, 21, 13, 30]
+    }];
+    const categories = [
+      "Mary", "Evans", "David", "Wilson", "Lily", "Roberts", "Julia", "May"
+    ]
+    this.studentsGenderRatio = this.commonBarGraph;
+    this.studentsGenderRatio.series = [...series];
+    this.studentsGenderRatio.xaxis.categories = [...categories];
   }
+
+  getTeachersGenderRatio() {
+    const series = [{
+      name: "Graphical",
+      data: [21, 22, 10, 28, 16, 21, 13, 30]
+    }];
+    const categories = [
+      "Mary", "Evans", "David", "Wilson", "Lily", "Roberts", "Julia", "May"
+    ]
+    this.teacherGenderRatio = this.commonBarGraph;
+    this.teacherGenderRatio.series = [...series];
+    this.teacherGenderRatio.xaxis.categories = [...categories];
+  }
+
+  getTypesOfSchools() {
+    const series = [44, 55, 41, 17, 15]
+    this.typesOfSchools = this.commonPieGraph;
+    this.typesOfSchools.series = [...series];
+  }
+
 }
 
