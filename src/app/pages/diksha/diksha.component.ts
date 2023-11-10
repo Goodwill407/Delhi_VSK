1426c3d3a8943841ee48b4cd939fd881f15587c5
unknown <devops@simplifiedschooling.in>
2023-11-10 15:10:25 +0530

    student Change

--- src/app/pages/diksha/diksha.component.ts
+++ src/app/pages/diksha/diksha.component.ts
@@ -14,12 +14,25 @@ export class DikshaComponent {
   allGrade: any;
   allSubject: any;
   allQrCoverage: any;
+ 
+  // dropdownModel
+  mediumModel:any=''
+  gradeModel:any=''
+  subjectModel:any=''
+
+  // graphName
+  TotalCountsGraph:any;
+  Average_no_of_playsGraph:any;
+  Average_play_timeGraph:any
+
+
 
   constructor(private httpService: HttpServiceService, private graphService: GraphService, private spinner: NgxSpinnerService) {
   }
 
   ngOnInit() {
     this.getAllCoverageQr();
+    this.getAllCounts_learningsession()
   }
 
   getAllCoverageQr() {
@@ -33,4 +46,59 @@ export class DikshaComponent {
     })
   }
 
+  getAllCounts_learningsession(){
+    const Data={
+      subject: "English",
+      grade: "Class 10",
+      medium: "English"
+    }
+    this.httpService.post('learningsession/counts-learningsession',Data).subscribe((data:any)=>{
+      if(data){
+        const AllCounts_learningsession=data
+
+        this.getTotalCountGraph(AllCounts_learningsession)
+        this.getTotalAverage_no_of_playsGraph(AllCounts_learningsession)
+        this.getTotalAverage_play_timeGraph(AllCounts_learningsession)
+
+      }
+    })
+  }
+
+  getTotalCountGraph(all_Data:any){
+    const LableName = all_Data.map((item:any)=>item._id)
+    const TotalCounts =all_Data.map((item:any)=>item.counts)
+    this.TotalCountsGraph = this.graphService.PieGraph('donut', '');
+    const series = TotalCounts;
+    const labels = LableName
+    this.TotalCountsGraph.series = [...series];
+    this.TotalCountsGraph.labels = [...labels]
+   
+
+  }
+
+  getTotalAverage_no_of_playsGraph(all_Data:any){
+    const LableName = all_Data.map((item:any)=>item._id)
+    const Average_no_of_playsCount =all_Data.map((item:any)=>item.average_no_of_plays)
+    this.Average_no_of_playsGraph = this.graphService.PieGraph('donut', '');
+    const series = Average_no_of_playsCount.map((item:any)=> parseFloat(item.toFixed(2)));
+    const labels = LableName
+    this.Average_no_of_playsGraph.series = [...series];
+    this.Average_no_of_playsGraph.labels = [...labels]
+    // this.Average_no_of_playsGraph.legend.formatter = function (val: any) { return val; }
+    
+
+  }
+
+  getTotalAverage_play_timeGraph(all_Data:any){
+    const LableName = all_Data.map((item:any)=>item._id)
+    const Average_play_timeCounts =all_Data.map((item:any)=>item.average_play_time)
+    this.Average_play_timeGraph = this.graphService.PieGraph('donut', '');
+    const series = Average_play_timeCounts.map((item:any)=> parseFloat(item.toFixed(2)));
+    const labels = LableName;
+    this.Average_play_timeGraph.series = [...series];
+    this.Average_play_timeGraph.labels = [...labels];
+    // this.Average_play_timeGraph.legend.formatter = function (val: any) { return val; }
+    
+
+  }
 }
