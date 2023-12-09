7c61c78dc3b79c09eb4820c3a87456cda77d6b30
unknown <devops@simplifiedschooling.in>
2023-12-09 10:18:52 +0530

    merging

--- src/app/pages/attendance/attendance.component.ts
+++ src/app/pages/attendance/attendance.component.ts
@@ -2,6 +2,7 @@ import { DatePipe } from '@angular/common';
 import { Component, SimpleChange, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 import { CommunicationService } from 'src/app/services/communication.service';
 import { GraphService } from 'src/app/services/graph-service.service';
 import { HttpServiceService } from 'src/app/services/http-service.service';
@@ -13,19 +14,53 @@ import { HttpServiceService } from 'src/app/services/http-service.service';
 })
 export class AttendanceComponent {
   communicationServiceMobile: any;
+  dateModel: any;
+  allData: any;
+  aided_graphData: any;
+  formattedDate: any;
 
-  constructor(private communicationService: CommunicationService) { 
+  constructor(private communicationService: CommunicationService, private httpService:HttpServiceService, public datepipe: DatePipe, private graphService: GraphService, private spinner: NgxSpinnerService, private toastr: ToastrService) { 
     this.communicationServiceMobile = this.communicationService.isMobile;
 
   }
 
   ngOnInit() {
-
+    this.dateModel = new Date();
+    this.dateModel.setDate(this.dateModel.getDate() - 1);
+    this.formattedDate = this.datepipe.transform(this.dateModel, 'dd-MMM-yyyy');
+    this.getDataAidedSchool();
   }
 
+  getDate(date: any) {
+    let Mdate: any;
+    return Mdate = this.datepipe.transform(date, 'yyyy-MM-dd');
+  }
   handleParentClick() {
     this.communicationService.emitParentClick();
   }
 
+  getDataAidedSchool(){
+    this.formattedDate = this.datepipe.transform(this.dateModel, 'dd-MMM-yyyy');
+    this.spinner.show();
+    this.httpService.post('attendance/attendancecout/aidedschools',{date:this.getDate(this.dateModel)}).subscribe(res =>{
+     if(res.attendanceCounts){
+      this.allData = res;
+      this.getAidedGraphData(res.attendanceCounts);
+      this.spinner.hide();
+     }else{
+      this.spinner.hide();
+      this.toastr.error('', 'Data not found for this date');
+     }
+    },error=>{
+      this.spinner.hide();
+      this.toastr.error('', 'Something Went Wrong ..!')
+    })
+  }
+
+  getAidedGraphData(data: any) {
+    this.aided_graphData = this.graphService.PieGraph('donut', ' Students');
+    this.aided_graphData.series = [data.totalPresentCount, data.totalAbsentCount, data.totalLeaveCount, data.totalAttendanceNotMarked];
+    this.aided_graphData.labels = ['Present', 'Absent', 'Leave','Not Mark'];
+  }
 
 }
